445fc70009e805f85e40048bcdb8b0d45d09071d
ed eustace <ed.eustace@gmail.com>
2019-02-26 09:28:16 +0000

    tweaks

--- pure-dj.js
+++ pure-dj.js
@@ -6,32 +6,28 @@ var j = 0;
 require("http").globalAgent.maxSockets = 2; //Infinity;
 const count = process.argv[2] || 80;
 const URL = "http://www.mocky.io/v2/5c6e99593400005200892dde";
-//const URL = "http://google.com";
 console.log("count:", count);
 console.time("loop");
-// Make 1000 parallel requests:
+
+const opts = {
+  headers: {
+    Connection: "Close"
+  }
+};
+
 for (i = 0; i < count; i++) {
   //const keepAliveAgent = new http.Agent({ keepAlive: true });
   http
-    .get(
-      URL,
-      {
-        headers: {
-          Connection: "Close"
-        }
-      },
-      function(res) {
-        console.log(j);
-        results.push(res.statusCode);
-        j++;
+    .get(URL, function(res) {
+      console.log(j);
+      results.push(res.statusCode);
+      j++;
 
-        if (j == i) {
-          // last request
-          console.timeEnd("loop");
-          process.exit();
-          //console.log(JSON.stringify(results));
-        }
+      if (j == i) {
+        // last request
+        console.timeEnd("loop");
+        process.exit();
       }
-    )
+    })
     .end();
 }
